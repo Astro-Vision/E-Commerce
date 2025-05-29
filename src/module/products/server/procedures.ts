b5c8f03fd50a88bfbe281c6559f60ee122a78b22
HandikaDiaz <handikaalexandriadiaz@gmail.com>
2025-05-30 05:24:43 +0700

    Filtering Products

--- src/module/products/server/procedures.ts
+++ src/module/products/server/procedures.ts
@@ -8,10 +8,25 @@ export const productRouter = createTRPCRouter({
         .input(
             z.object({
                 category: z.string().nullable().optional(),
+                minPrice: z.string().nullable().optional(),
+                maxPrice: z.string().nullable().optional(),
             })
         )
         .query(async ({ ctx, input }) => {
             const where: Where = {};
+
+            if (input.minPrice) {
+                where.price = {
+                    greater_than_equal: input.minPrice
+                }
+            }
+
+
+            if (input.maxPrice) {
+                where.price = {
+                    less_than_equal: input.maxPrice
+                }
+            }
 
             if (input.category) {
                 const categoriesData = await ctx.db.find({
@@ -40,11 +55,12 @@ export const productRouter = createTRPCRouter({
                     subcategoriesSlugs.push(
                         ...parentCategory.subcategories.map((subcategory) => subcategory.slug)
                     );
+
+                    where["category.slug"] = {
+                        in: [parentCategory.slug, ...subcategoriesSlugs]
+                    };
                 };
 
-                where["category.slug"] = {
-                    in: [parentCategory.slug, ...subcategoriesSlugs]
-                }
             };
 
             const data = await ctx.db.find({
