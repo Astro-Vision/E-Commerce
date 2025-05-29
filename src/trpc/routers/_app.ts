6fbcabc88e0783b6ed3541419f0ca7709fad3885
HandikaDiaz <handikaalexandriadiaz@gmail.com>
2025-05-29 07:56:17 +0700

    Products

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,11 @@
 import { categoriesRouter } from '@/module/categories/server/procedures';
+import { productRouter } from '@/module/products/server/procedures';
 import { createTRPCRouter } from '../init';
 import { authRouter } from '@/module/auth/server/procedures';
 
 export const appRouter = createTRPCRouter({
     auth: authRouter,
+    products: productRouter,
     categories: categoriesRouter
 });
 // export type definition of API
