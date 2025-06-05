fec434fe17cf40880569b9d44dd7cd0bc8d1463a
HandikaDiaz <handikaalexandriadiaz@gmail.com>
2025-06-05 07:51:38 +0700

    Tenant Pages

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -3,12 +3,14 @@ import { productRouter } from '@/module/products/server/procedures';
 import { createTRPCRouter } from '../init';
 import { authRouter } from '@/module/auth/server/procedures';
 import { tagsRouter } from '@/module/tags/server/procedures';
+import { tenantsRouter } from '@/module/tenants/server/procedures';
 
 export const appRouter = createTRPCRouter({
     auth: authRouter,
     tags: tagsRouter,
+    tenants: tenantsRouter,
     products: productRouter,
-    categories: categoriesRouter
+    categories: categoriesRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
