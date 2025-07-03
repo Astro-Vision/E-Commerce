f20b8c1e1377663f5d3712661829a7993d4fe2d9
HandikaDiaz <handikaalexandriadiaz@gmail.com>
2025-07-03 19:51:13 +0700

    Library

--- src/module/checkout/hook/use-cart.ts
+++ src/module/checkout/hook/use-cart.ts
@@ -1,30 +1,43 @@
+import { useCallback } from "react";
 import { useCartStore } from "../store/use-cart-store"
+import { useShallow } from "zustand/react/shallow"
 
 export const useCart = (tenantSlug: string) => {
-    const { addProduct, removeProduct, clearCart, clearAllCarts, getCartByTenant } = useCartStore()
+    const addProduct = useCartStore((state) => state.addProduct);
+    const removeProduct = useCartStore((state) => state.removeProduct);
+    const clearCart = useCartStore((state) => state.clearCart);
+    const clearAllCarts = useCartStore((state) => state.clearAllCarts);
 
-    const productIds = getCartByTenant(tenantSlug);
+    const productIds = useCartStore(useShallow((state) => state.tenantCarts[tenantSlug]?.productIds || []));
 
-    const toggleProduct = (proudctId: string) => {
+    const toggleProduct = useCallback((proudctId: string) => {
         if (productIds.includes(proudctId)) {
             removeProduct(tenantSlug, proudctId);
         } else {
             addProduct(tenantSlug, proudctId);
         }
-    };
+    }, [addProduct, removeProduct, productIds, tenantSlug]);
 
-    const isProductInCart = (productId: string) => {
+    const isProductInCart = useCallback((productId: string) => {
         return productIds.includes(productId);
-    };
+    }, [productIds]);
 
-    const clearTenantCart = () => {
+    const clearTenantCart = useCallback(() => {
         clearCart(tenantSlug);
-    };
+    }, [clearCart, tenantSlug]);
+
+    const handleAddProduct = useCallback((productId: string) => {
+        addProduct(tenantSlug, productId);
+    }, [addProduct, tenantSlug]);
+
+    const handleRemoveProduct = useCallback((productId: string) => {
+        removeProduct(tenantSlug, productId);
+    }, [removeProduct, tenantSlug]);
 
     return {
         productIds,
-        addProduct: (productId: string)  => addProduct(tenantSlug, productId),
-        removeProduct: (productId: string)  => removeProduct(tenantSlug, productId),
+        addProduct: handleAddProduct,
+        removeProduct: handleRemoveProduct,
         clearCart: clearTenantCart,
         clearAllCarts,
         toggleProduct,
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -5,11 +5,13 @@ import { authRouter } from '@/module/auth/server/procedures';
 import { tagsRouter } from '@/module/tags/server/procedures';
 import { tenantsRouter } from '@/module/tenants/server/procedures';
 import { checkoutRouter } from '@/module/checkout/server/procedures';
+import { libraryRouter } from '@/module/library/server/procedures';
 
 export const appRouter = createTRPCRouter({
     auth: authRouter,
     tags: tagsRouter,
     tenants: tenantsRouter,
+    library: libraryRouter,
     products: productRouter,
     checkout: checkoutRouter,
     categories: categoriesRouter,
